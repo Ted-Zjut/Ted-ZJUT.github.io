4206102027400b0ae3d0586041d9739c6e21cbdd
Ted Wang <ted0703@gmail.com>
2015-07-13 02:14:16 +0800

    Site updated: 2015-07-13 02:14:15

--- js/fancy-box.js
+++ js/fancy-box.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
       $imageWrapLink = $image.wrap('<a href="' + this.getAttribute('src') + '"></a>').parent('a');
     }
     $imageWrapLink.addClass('fancybox');
-    $imageWrapLink.attr("title",this.title);
+    if(this.title){
+      $imageWrapLink.attr("title",this.title); //make sure img title tag will show correctly in fancybox
+    }
   });
 });
 $('.fancybox').fancybox({
